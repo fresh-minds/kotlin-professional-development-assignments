cfd0215e0cc3f2182ae2dafe5b8f259b888fdce1
Bas de Groot <bdegroot@protonmail.com>
2023-03-15 16:26:15 +0100

    First assignments module 03

---
+++ module-03-collections-and-arrays/src/main/kotlin/nl/freshminds/Assignment01.kt
@@ -0,0 +1,45 @@
+package nl.freshminds
+
+/**
+ * Find the amount of races held in the 2023 season
+ */
+fun amountOfRacesIn2023(races: List<Race>): Int {
+    TODO()
+}
+
+/**
+ * Return all unique circuit names
+ */
+fun circuitNames(races: List<Race>): Set<String> {
+    TODO()
+}
+
+/**
+ * Return true if all races are from a given season
+ */
+fun checkAllRacesAreFromSeason(races: List<Race>, season: Int): Boolean {
+    TODO()
+}
+
+/**
+ * Return true if there is at least one driver with a given nationality
+ */
+fun hasDriverWithNationality(drivers: List<Driver>, nationality: String): Boolean {
+    TODO()
+}
+
+/**
+ * Return the number of results for a given status
+ */
+fun countResultsByStatus(race: Race, status: String): Int {
+    TODO()
+}
+
+/**
+ * Return a driver who has a given nationality, or null if there is none
+ */
+fun findDriverFrom(drivers: List<Driver>, nationality: String): Driver? {
+    TODO()
+}
+
+// Drivers and constructor association
