6cbdce6e367d1d1b454509a32f1bde3d87f4de09
Bas de Groot <bdegroot@protonmail.com>
2023-04-11 19:49:09 +0200

    Bump JUnit version

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ allprojects {
     dependencies {
         implementation(kotlin("stdlib-jdk8"))
 
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
         testImplementation(kotlin("test"))
     }
 }
