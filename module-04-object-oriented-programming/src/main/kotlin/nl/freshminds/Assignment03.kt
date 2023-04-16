c730838b336882499dca8dd28ddcbd64e4510386
Bas de Groot <bdegroot@protonmail.com>
2023-04-16 12:12:41 +0200

    First assignments for module 04

---
+++ module-04-object-oriented-programming/src/main/kotlin/nl/freshminds/Assignment03.kt
@@ -0,0 +1,16 @@
+package nl.freshminds
+
+/**
+ * Refactor the [Service] class in such a way that the [restTemplate] can be declared as 'private val' instead
+ * of 'private lateinit var'.
+ */
+class Service {
+    private lateinit var restTemplate: RestTemplate
+
+    fun doSomething() = restTemplate.fetch()
+}
+
+// Fake restTemplate. Do not change this.
+class RestTemplate {
+    fun fetch(): String = TODO()
+}
---
+++ module-04-object-oriented-programming/src/main/kotlin/nl/freshminds/Assignment04.kt
@@ -0,0 +1,17 @@
+package nl.freshminds
+
+/**
+ * 1. Create a [Camera] interface with 2 methods: [takePicture] and [autoAdjustWhiteBalance]
+ *
+ * 2. Create a class [Iphone] which accepts a property [type] (11, 12, 13, etc) in the primary constructor.
+ *
+ * 3. Make [Iphone] implement [Camera]. For the implementations just print some text to the console. For example:
+ * "Took picture with an Iphone 13" or "Auto adjusted white balance on the Iphone 12".
+ *
+ * 4. Create a class [Dslr] which accepts a property [model] (Canon D600, Nikon D7500, etc)
+ *
+ * 5. Make [Dslr] implement the [Camera] interface.
+ *
+ * 6. Define a main function and create a list of cameras by creating different variants of [Iphone] and [Dslr].
+ * Loop over each element in this list and call both [takePicture] and [autoAdjustWhiteBalance] for each element.
+ */
