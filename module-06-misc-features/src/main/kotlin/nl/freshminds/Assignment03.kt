6d64fc0791dc3df8e1eeba2e2513fe29f69b72bc
Bas de Groot <bdegroot@protonmail.com>
2023-04-26 18:43:40 +0200

    More assignments KF-06

---
+++ module-06-misc-features/src/main/kotlin/nl/freshminds/Assignment03.kt
@@ -0,0 +1,14 @@
+package nl.freshminds
+
+/**
+ * There are no tests available for this assignment.
+ *
+ * 1. Create an infix function [isMultipleOf] that takes two integers as arguments and returns true
+ * if the first integer is a multiple of the second integer.
+ *
+ * 2. Create a class Student with two properties: name (String) and grade (Int). Create an infix function
+ * [higherGradeThan] that takes two Student objects as arguments and returns true if the first student's
+ * grade is higher than the second student's grade.
+ *
+ * 3. Create a main function and give your infix functions a try.
+ */
