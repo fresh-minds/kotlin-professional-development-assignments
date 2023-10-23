5296e60d12115bce8e6f3aa22db8955d1a807cdd
Bas de Groot <bdegroot@protonmail.com>
2023-10-23 16:37:23 +0200

    Fix typo

--- module-04-object-oriented-programming/src/main/kotlin/nl/freshminds/Oop05.kt
+++ module-04-object-oriented-programming/src/main/kotlin/nl/freshminds/Oop05.kt
@@ -6,7 +6,7 @@ import java.time.LocalDate
  * 1. Write an extension function on [LocalDate] with the name [isBetween]. This function takes 2 LocalDates as inputs
  * and returns true if the receiver object is a date between these 2 dates. Returns false otherwise.
  *
- * 2. Write an extesion function on [List<Int>] with the name [squareNumbers]. This function should square each
+ * 2. Write an extension function on [List<Int>] with the name [squareNumbers]. This function should square each
  * element in the list. So if the original list contains [2,3,4,5] the resulting list contains [4, 9, 16, 25].
  *
  * 3. Write an extension function on the [String] class with the name [isPalindrome]. This function returns true
