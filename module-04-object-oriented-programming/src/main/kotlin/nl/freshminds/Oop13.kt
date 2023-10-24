4d394f13bd259936c9a9d83be09cc8b958d03ac8
Bas de Groot <bdegroot@protonmail.com>
2023-10-24 15:39:10 +0200

    Fix typo

--- module-04-object-oriented-programming/src/main/kotlin/nl/freshminds/Oop13.kt
+++ module-04-object-oriented-programming/src/main/kotlin/nl/freshminds/Oop13.kt
@@ -3,7 +3,7 @@ package nl.freshminds
 /**
  * There are no tests available for this assignment.
  *
- * Implement a singleton [IdGenerator]. It should hava single method [next] which returns the next random ID as a
+ * Implement a singleton [IdGenerator]. It should have a single method [next] which returns the next random ID as a
  * String. Next accepts a single argument of [prefix] which is added to the generated ID as a prefix. If no [prefix]
  * is specified, it defaults to an empty string.
  */
